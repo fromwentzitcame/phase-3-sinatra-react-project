fee431e031e55b2d62585c22f354f8be57fd6da3
Alex Wentz <wentz.alex@gmail.com>
2021-11-17 11:00:21 -0500

    finished-delete

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -2,9 +2,9 @@ class Newsletter < ActiveRecord::Base
 
     has_many :subscribers
     has_many :customers, through: :subscribers
-
+    
     def customer_list
         self.subscribers.map{|sub| sub.customer}
     end
-    
+
 end
